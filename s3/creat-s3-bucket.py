034f1267d471197bafd63365ed93e06cf7992ad5
Anchal Agrahari <anchalagrahari258@gmail.com>
2025-04-05 22:33:31 +0530

    create_s3_bucket

---
+++ s3/creat-s3-bucket.py
@@ -0,0 +1,23 @@
+import boto3
+def create_s3_bucket(bucket_name):
+    try:
+        client = boto3.client('s3')
+        response = client.create_bucket(
+            Bucket_name = bucket_name,
+            CreateBucketConfiguration={
+                'LocationConstraint':'us-west-2',
+            },)
+    except Exception as e:
+        print("Exception occured", e)
+
+
+if __name__ == '__main__':
+    try:
+        bucket_name=input("Enter the uniqe:").stript()
+        if len(bucket_name) != 0:
+            create_s3_bucket(bucket_name)
+        print("Please enter the uniqe bucket name")
+    except Exception as e:
+        print("Exception occured",e)
+
+    
